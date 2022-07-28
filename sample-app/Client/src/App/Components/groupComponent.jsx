94481b69898eb49a8c92d7b9c6b8d9a2c2c8a425
sheruaravindreddy <sheruaravindreddy@gmail.com>
2022-07-28 13:50:56 -0400

    adding logout functoanlity

--- sample-app/Client/src/App/Components/groupComponent.jsx
+++ sample-app/Client/src/App/Components/groupComponent.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { useNavigate } from "react-router-dom";
-import { Button, Container } from "reactstrap";
+import { Container } from "reactstrap";
 
 function GroupComponent(props) {
   let navigate = useNavigate();
--- sample-app/Client/src/App/Controllers/navBarController.jsx
+++ sample-app/Client/src/App/Controllers/navBarController.jsx
@@ -1,21 +1,26 @@
 import React from "react";
-import { Component } from "react";
 import ImageComponent from "../Components/GroupSubComponents/ImageComponent";
 
-class NavBar extends Component {
-  state = {};
-  render() {
+export default function NavBar() {
+
+  const logout = () => {
+    console.debug("Logging out...");
+    window.localStorage.clear();
+    window.location.reload(true);
+  }  
+
     return (
       <div className="logo_logout">
         <div className="logo">
+          
           <ImageComponent src={require("../../images/logo.png")} />
+          
         </div>
         <div className="logout">
+          <a onClick={logout}>
           <ImageComponent src={require("../../images/logout.png")} />
+          </a>
         </div>
       </div>
     );
-  }
-}
-
-export default NavBar;
+    }
