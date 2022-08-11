edecdec32737a494afd6ce357197f58ff47e603e
aravindsherusonos <107195536+aravindsherusonos@users.noreply.github.com>
2022-08-11 14:21:59 -0400

    case sensitive changes

---
+++ sample-app/Client/src/App/Components/GroupSubComponents/imageComponent.jsx
@@ -0,0 +1,17 @@
+/**
+ * @author Mugdha Rane <mugdha.rane@sonos.com>
+ * Code to display images, pass the url of the image as props
+ */
+
+import React, { Component } from 'react';
+
+class ImageComponent extends Component {
+    
+    render() {
+        return (
+            <img width="100%" height="100%" src={this.props.src} alt={this.props.alt} />
+        );
+    }
+}
+
+export default ImageComponent;
---
+++ sample-app/Client/src/App/Components/GroupSubComponents/playbackMetaDataComponent.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Component } from "react";
+import HelperControls from "../../ControlAPIs/playerControls";
+import PlayBackMetadata from "../../ControlAPIs/playbackMetadata";
+import ImageComponent from "./imageComponent";
+import { SocketContext, socket } from "../../WebSocket/socket";
+import PlayBackMetaDataEvent from "../../WebSocket/playbackMetaDataEvent";
+
+class PlayBackMetaDataComponent extends Component {
+  constructor() {
+    super();
+    this.ControlOptions = new HelperControls();
+    this.volumeSlider = React.createRef();
+    this.state = {
+      getPlayBackMetaDataFlag: true,
+      trackName: null,
+      albumName: null,
+      trackImage: null,
+    };
+  }
+
+  playBackMetadataHandler = (flag, trackName, albumName, trackImage) => {
+    this.setState({
+      getPlayBackMetaDataFlag: flag,
+      trackName: trackName,
+      albumName: albumName,
+      trackImage: trackImage,
+    });
+  };
+
+  getImage = () => {
+    if (this.state.trackImage === undefined || this.state.trackImage === "") {
+      return require("../../../images/sonos.png");
+    } else {
+      return this.state.trackImage;
+    }
+  };
+
+  receiveEventsHandler = (response) => {
+    console.log(response);
+    if (this.state.trackName !== response["trackName"]) {
+      this.setState({
+        trackName: response["trackName"],
+        albumName: response["albumName"],
+        trackImage: response["trackImage"],
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="play_back_metadata">
+        <SocketContext.Provider value={socket}>
+          <PlayBackMetaDataEvent handler={this.receiveEventsHandler} />
+        </SocketContext.Provider>
+
+        {this.state.getPlayBackMetaDataFlag && (
+          <PlayBackMetadata
+            group_id={this.props.groupID}
+            playBackMetadataHandler={this.playBackMetadataHandler}
+            museClientConfig={this.props.museClientConfig}
+          />
+        )}
+        <div className="track_details">
+          <div className="track_image">
+            <ImageComponent src={this.getImage()} alt="Song being played" />
+            <div className="track_name">{this.state.trackName}</div>
+            <div className="track_artist">{this.state.albumName}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PlayBackMetaDataComponent;
