11862d774ec153370c9ca56754262769e1860152
olamidekumapayisonos <135860919+olamidekumapayisonos@users.noreply.github.com>
2023-07-11 18:22:38 -0400

    SWPBL-210293 added back button to UI (#44)
    * SWPBL-210293 added back button to UI
    
    * functioning back button
    
    * changes in location of button
    
    * changes in location of button
    
    * formatting and making a back button component instead
    
    * unnecessary comments
    
    * formatting
    
    * formatting
    
    * formatting

--- sample-app/Client/src/App/Components/groupPlayersComponent.jsx
+++ sample-app/Client/src/App/Components/groupPlayersComponent.jsx
@@ -8,6 +8,7 @@ import PlayBackStateButtonWrapper from "./GroupSubComponents/PlaybackStateButton
 import VolumeComponentWrapper from "./GroupSubComponents/VolumeComponentWrapper";
 import PlayersController from "../Controllers/playersController";
 import HeaderComponent from "./headerComponent";
+import BackButton from "./backButtonComponent"
 
 class GroupPlayersComponent extends Component {
   constructor(props) {
@@ -30,6 +31,9 @@ class GroupPlayersComponent extends Component {
         <HeaderComponent />
 
         <div className="group_name">
+          <div className="back_button_Wrapper"> 
+            <BackButton />
+          </div>
           <div className="group_box">
             <p>{this.group.name} </p>
           </div>
--- sample-app/Client/src/App/Components/listGroupsComponent.jsx
+++ sample-app/Client/src/App/Components/listGroupsComponent.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Component } from "react";
 import GroupRoutingController from "../Controllers/groupRoutingController";
 import HeaderComponent from "./headerComponent";
+import BackButton from "./backButtonComponent"
 
 class ListGroupsComponent extends Component {
   state = {};
@@ -11,8 +12,13 @@ class ListGroupsComponent extends Component {
       <div>
         <div className="main_page">
           <HeaderComponent />
-          <div className="group_text">
-            <p>List of Groups in your Household: </p>
+          <div className="info_Wrapper">
+            <div className="back_button_Wrapper"> 
+                <BackButton />
+            </div>
+            <div className="group_text">
+              <p>List of Groups in your Household: </p>
+            </div>
           </div>
           {this.props.groups.map((group) => (
             <GroupRoutingController
