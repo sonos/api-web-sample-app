b9265e9c467e7e9532fd370603f41f0f180de8c7
Olamide <olamide.kumapayi@sonos.com>
2023-08-21 16:14:32 -0400

    changes

--- sample-app/Test-cors/mock-cors.js
+++ sample-app/Test-cors/mock-cors.js
@@ -1,27 +1,22 @@
 const http = require('http');
 const { url } = require('inspector');
-const { readFile } = require('fs/promises')
+const fs = require('fs')
 
 const hostname = '127.0.0.1'
 const port = 8090
-/**
- * Returns the content of the given file `path` as a string.
- * @param {String} path
- * @returns {String}
- */
-function content(path) {  
-    return readFile(path, 'utf8')
-}
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     if(req.url.includes('groups'))
     {
-        content('./group.txt');
+        //this is a mock group response statement
+        const fileContent = fs.readFileSync('./group.txt', 'utf-8');
+        res.end(fileContent);
     } else {
         //this is a mock household response statement 
-        res.end("{\n \t \"households\": [\n \t \t {\n \t \t \t \"id\": \"Sonos_iHJBfvUzznmHiuYmP2aT18usVp.NoCK8hTDBL4BUwR8k2Fe\", \n \t \t \t \"name\": null, \n \t \t \t \"ownerLuid\": \"364896036\" \n \t \t } \n \t ] \n }");
+        const fileContent = fs.readFileSync('./household.txt', 'utf-8');
+        res.end(fileContent);
     }
     
 });
